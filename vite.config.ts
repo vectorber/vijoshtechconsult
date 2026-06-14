382a813b8c34b3ed27acf170fdfd46f57f77396a
vectorber <36734312+vectorber@users.noreply.github.com>
2026-06-14 15:13:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import vercel from '@vercel/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
+    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    netlify(),
+    vercel(),
     tanstackStart(),
     viteReact(),
   ],
